5b255ff018fe57f8756e755d25b1e5991bded368
Rohit10-01-1999 <120014851+Rohit10-01-1999@users.noreply.github.com>
2022-12-09 17:57:02 +0530

    Add files via upload

---
+++ projecttest/GreatestNumber.java
@@ -0,0 +1,19 @@
+package projecttest;
+
+public class GreatestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num1=10, num2=20, num3=7;
+if(num1 >= num2 && num1 >= num3)
+	System.out.println(num1+" is the greatest number");
+ 
+else if(num2 >= num1 && num2 >= num2)
+	System.out.println(num2+" is the greatest number");
+
+else
+	System.out.println(num3+ "is the greatest number");
+	
+	}
+
+}
